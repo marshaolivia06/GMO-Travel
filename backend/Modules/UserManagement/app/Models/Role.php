5147e7e31e5022dbf0057154d2b74849bacffe9b
marshaolivia06 <marshaolivia2006@gmail.com>
2026-09-16 15:04:03 +0700

    feat(user-management): add status to role

--- backend/Modules/UserManagement/app/Models/Role.php
+++ backend/Modules/UserManagement/app/Models/Role.php
@@ -12,5 +12,10 @@ class Role extends SpatieRole
     protected $fillable = [
         'name',
         'guard_name',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
     ];
 }
