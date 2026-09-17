6cbabeb99cd538c38a1d2609e77ec02d8a0e542e
marshaolivia06 <marshaolivia2006@gmail.com>
2026-09-17 09:52:39 +0700

    feat(master-management): add category and department routes

--- backend/Modules/MasterManagement/routes/api.php
+++ backend/Modules/MasterManagement/routes/api.php
@@ -1,31 +1,66 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\MasterManagement\Http\Controllers\MasterManagementController;
+use Modules\MasterManagement\Http\Controllers\CategoryController;
+use Modules\MasterManagement\Http\Controllers\DepartmentController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+
+    // CATEGORIES
     Route::get('master-management/categories', [
-        MasterManagementController::class,
-        'categoriesIndex',
+        CategoryController::class,
+        'index',
     ]);
 
     Route::post('master-management/categories', [
-        MasterManagementController::class,
-        'categoriesStore',
+        CategoryController::class,
+        'store',
     ]);
 
-    Route::get('master-management/categories/{category}', [
-        MasterManagementController::class,
-        'categoriesShow',
+    Route::get('master-management/categories/{id}', [
+        CategoryController::class,
+        'show',
     ]);
 
-    Route::put('master-management/categories/{category}', [
-        MasterManagementController::class,
-        'categoriesUpdate',
+    Route::put('master-management/categories/{id}', [
+        CategoryController::class,
+        'update',
     ]);
 
-    Route::delete('master-management/categories/{category}', [
-        MasterManagementController::class,
-        'categoriesDestroy',
+    Route::delete('master-management/categories/{id}', [
+        CategoryController::class,
+        'destroy',
     ]);
+
+    // DEPARTMENTS
+    Route::get('master-management/departments/options', [
+        DepartmentController::class,
+        'options',
+    ]);
+
+    Route::get('master-management/departments', [
+        DepartmentController::class,
+        'index',
+    ]);
+
+    Route::post('master-management/departments', [
+        DepartmentController::class,
+        'store',
+    ]);
+
+    Route::get('master-management/departments/{id}', [
+        DepartmentController::class,
+        'show',
+    ]);
+
+    Route::put('master-management/departments/{id}', [
+        DepartmentController::class,
+        'update',
+    ]);
+
+    Route::delete('master-management/departments/{id}', [
+        DepartmentController::class,
+        'destroy',
+    ]);
+
 });
