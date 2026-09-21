fbd4a10aa4437da3a67ce56f836258c8a942722e
marshaolivia06 <marshaolivia2006@gmail.com>
2026-09-21 13:19:29 +0700

    refactor(master-management): update api and web routes

--- backend/Modules/MasterManagement/routes/api.php
+++ backend/Modules/MasterManagement/routes/api.php
@@ -1,99 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 use Modules\MasterManagement\Http\Controllers\CategoryController;
 use Modules\MasterManagement\Http\Controllers\DepartmentController;
 use Modules\MasterManagement\Http\Controllers\SectionController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+Route::middleware(['auth:sanctum'])
+    ->prefix('v1')
+    ->group(function () {
 
-    // CATEGORIES
-    Route::get('master-management/categories', [
-        CategoryController::class,
-        'index',
-    ]);
+        // CATEGORIES
+        Route::apiResource(
+            'master-management/categories',
+            CategoryController::class
+        );
 
-    Route::post('master-management/categories', [
-        CategoryController::class,
-        'store',
-    ]);
+        // DEPARTMENTS
+        Route::get(
+            'master-management/departments/options',
+            [DepartmentController::class, 'options']
+        );
 
-    Route::get('master-management/categories/{id}', [
-        CategoryController::class,
-        'show',
-    ]);
+        Route::apiResource(
+            'master-management/departments',
+            DepartmentController::class
+        );
 
-    Route::put('master-management/categories/{id}', [
-        CategoryController::class,
-        'update',
-    ]);
+        // SECTIONS
+        Route::get(
+            'master-management/sections/options',
+            [SectionController::class, 'options']
+        );
 
-    Route::delete('master-management/categories/{id}', [
-        CategoryController::class,
-        'destroy',
-    ]);
-
-    // DEPARTMENTS
-    Route::get('master-management/departments/options', [
-        DepartmentController::class,
-        'options',
-    ]);
-
-    Route::get('master-management/departments', [
-        DepartmentController::class,
-        'index',
-    ]);
-
-    Route::post('master-management/departments', [
-        DepartmentController::class,
-        'store',
-    ]);
-
-    Route::get('master-management/departments/{id}', [
-        DepartmentController::class,
-        'show',
-    ]);
-
-    Route::put('master-management/departments/{id}', [
-        DepartmentController::class,
-        'update',
-    ]);
-
-    Route::delete('master-management/departments/{id}', [
-        DepartmentController::class,
-        'destroy',
-    ]);
-
-   // SECTIONS
-Route::get('master-management/sections/options', [
-    SectionController::class,
-    'options',
-]);
-
-Route::get('master-management/sections', [
-    SectionController::class,
-    'index',
-]);
-
-Route::post('master-management/sections', [
-    SectionController::class,
-    'store',
-]);
-
-Route::get('master-management/sections/{id}', [
-    SectionController::class,
-    'show',
-]);
-
-Route::put('master-management/sections/{id}', [
-    SectionController::class,
-    'update',
-]);
-
-Route::delete('master-management/sections/{id}', [
-    SectionController::class,
-    'destroy',
-]);
-
-
-});
+        Route::apiResource(
+            'master-management/sections',
+            SectionController::class
+        );
+    });
